e6129b445b6a3dfcea899d8d7ece69c125b4411f
basherru <dr.bazhenoff2017@yandex.ru>
2019-02-08 16:10:30 +0600

    Add files via upload

--- gtfo_store.sh
+++ gtfo_store.sh
@@ -1,3 +1,4 @@
+find . -name .DS_Store -print0 | xargs -0 git rm --ignore-unmatch --cached
 find . -name .DS_Store -print0 | xargs -0 git rm --ignore-unmatch
 grep -q -x -F ".DS_Store" ~/.gitignore_global || echo ".DS_Store" >> ~/.gitignore_global
 grep -q -x -F "._.DS_Store" ~/.gitignore_global || echo "._.DS_Store" >> ~/.gitignore_global
